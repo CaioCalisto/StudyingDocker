56a1b165a31dd935900fb13d4108d8d6bd130be1
Caio Calisto <caio.calisto@360imprimir.pt>
2019-02-15 11:25:32 +0000

    Brand MongoDB

---
+++ src/api/MyFirstAPI.Api/Controllers/BrandController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private IMediator mediator;
+
+
+    }
+}
---
+++ src/api/MyFirstAPI.Application/MongoDB/Commands/CreateBrandCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using MyFirstAPI.Domain;
+using MyFirstAPI.Domain.Ports;
+
+namespace MyFirstAPI.Application.MongoDB.Commands
+{
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, bool>
+    {
+        private IBrandRepository brandRepository;
+
+        public CreateBrandCommandHandler(IBrandRepository brandRepository)
+        {
+            this.brandRepository = brandRepository;
+        }
+
+        public async Task<bool> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            Brand brand = new Brand()
+            {
+                Name = request.Name,
+                Country = request.Country
+            };
+            this.brandRepository.Add(brand);
+            await this.brandRepository.UnitOfWork.SaveEntitiesAsync();
+            return true;
+        }
+    }
+}
