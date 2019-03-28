bea7290a78171f716d36e8bd637bc8b859c31e56
Caio Cesar Calisto <caiocesarcalisto@gmail.com>
2019-03-28 10:04:30 +0000

    Using RabbitMQ

---
+++ src/RabbitMQ/Receive/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnection connection = null;
+            string queueName = "SimpleSendReceive_QUEUE";
+            bool tryAgain = true;
+            while (tryAgain == true)
+            {
+                try
+                {
+                    connection = CreateConnection();
+                    tryAgain = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Thread.Sleep(3000);
+                }
+            }
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            consumer.Received += ConsumerReceived;
+            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+            while(true == true)
+            {
+                Console.WriteLine("I am listening...");
+                Thread.Sleep(10000);
+            } 
+        }
+
+        static IConnection CreateConnection()
+        {
+            IConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "rabbitmq",
+                UserName = "guest",
+                Password = "guest",
+                Port = 5672
+            };
+            return factory.CreateConnection();
+        }
+
+        private static void ConsumerReceived(object sender, BasicDeliverEventArgs args)
+        {
+            string mensagem = Encoding.UTF8.GetString(args.Body);
+            Console.WriteLine($"Message RECEIVED: {mensagem}");
+        }
+    }
+}
---
+++ src/RabbitMQ/Send/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnection connection = null;
+            bool tryAgain = true;
+            while(tryAgain == true)
+            {
+                try
+                {
+                    connection = CreateConnection();
+                    tryAgain = false;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Thread.Sleep(3000);
+                }
+            }
+
+            bool continueSending = true;
+            while(continueSending == true)
+            { 
+                string mensagem = $"New message {DateTime.Now}";
+                string queueName = "SimpleSendReceive_QUEUE";
+
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    byte[] body = Encoding.UTF8.GetBytes(mensagem);
+                    channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+                    Console.WriteLine($"Message SENT: {mensagem}");
+                }
+                Thread.Sleep(10000);
+            }
+        }
+
+        static IConnection CreateConnection()
+        {
+            IConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "rabbitmq",
+                UserName = "guest",
+                Password = "guest",
+                Port = 5672
+            };
+            return factory.CreateConnection();
+        }
+    }
+}
