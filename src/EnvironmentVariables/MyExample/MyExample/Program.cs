250333697c7176c3cb8502a43c9c33fa19ee7b33
Caio Calisto <caio.calisto@360imprimir.pt>
2019-04-01 17:50:09 +0100

    Exemplo com Variavel de ambiente

---
+++ src/EnvironmentVariables/MyExample/MyExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myVariableValue = Environment.GetEnvironmentVariable("MyCustomVariable");
+            Console.WriteLine(myVariableValue);
+        }
+    }
+}
